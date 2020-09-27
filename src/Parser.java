ca8d1a4de86c76423a67b0819915e8547fc05da4
Ken Jiang <073686289@gapps.yrdsb.ca>
2020-09-27 01:18:57 -0400

    now exports MIDI!!!!

--- src/Parser.java
+++ src/Parser.java
@@ -15,7 +15,7 @@ public class Parser {
     static final int KEY_WIDTH = 12;
     static final int IMAGE_WIDTH = 640;
     static final int KEYS = 54;
-    static final int FRAMES = 3911; //3911 is the max frames
+    static final int FRAMES = 200; //3911 is the max frames
 
     public static Color[] parseImage(File file) throws Exception{
         Color[] temp = new Color[KEYS];
@@ -44,11 +44,11 @@ public class Parser {
         Color[] originalColors = parseImage(new File("frame0.png"));
         String[] letters = {"Ab", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G",};
         NoteContainer nc = new NoteContainer(KEYS);
-        String s = "0000";
+        String str = "0000";
         for (int x = 0; x < FRAMES; x++) {
             System.out.print(df.format((x*1.0/(FRAMES - 1))*100) + "%\r");
-            s = countUp(s);
-            Color[] frameColors = parseImage(new File("frames\\" + s + ".jpg"));
+            str = countUp(str);
+            Color[] frameColors = parseImage(new File("frames\\" + str + ".jpg"));
 
             for (int i = 0; i < originalColors.length; i++) {
                 Color og = originalColors[i];
@@ -64,7 +64,6 @@ public class Parser {
                 }
             }
         }
-
         System.out.println("Processing Done");
 
         System.out.println("Starting image creation");
@@ -78,8 +77,71 @@ public class Parser {
                 }
             }
         }
-        System.out.println("Image written");
         ImageIO.write(output,"png", new File("output.png"));
+        System.out.println("Image written");
+
+        System.out.println("Initializing midi writer");
+
+        Sequence s = new Sequence(javax.sound.midi.Sequence.PPQ,24);
+        Track t = s.createTrack();
+        byte[] b = {(byte)0xF0, 0x7E, 0x7F, 0x09, 0x01, (byte)0xF7};
+        SysexMessage sm = new SysexMessage();
+        sm.setMessage(b, 6);
+        MidiEvent me = new MidiEvent(sm,(long)0);
+        t.add(me);
+        MetaMessage mt = new MetaMessage();
+        byte[] bt = {0x02, (byte)0x00, 0x00};
+        mt.setMessage(0x51 ,bt, 3);
+        me = new MidiEvent(mt,(long)0);
+        t.add(me);
+        mt = new MetaMessage();
+        String TrackName = new String("midifile track");
+        mt.setMessage(0x03 ,TrackName.getBytes(), TrackName.length());
+        me = new MidiEvent(mt,(long)0);
+        t.add(me);
+        ShortMessage mm = new ShortMessage();
+        mm.setMessage(0xB0, 0x7D,0x00);
+        me = new MidiEvent(mm,(long)0);
+        t.add(me);
+        mm = new ShortMessage();
+        mm.setMessage(0xB0, 0x7F,0x00);
+        me = new MidiEvent(mm,(long)0);
+        t.add(me);
+        mm = new ShortMessage();
+        mm.setMessage(0xC0, 0x00, 0x00);
+        me = new MidiEvent(mm,(long)0);
+        t.add(me);
+        System.out.println("Midi writer initialized");
+        for (int f = 1; f < FRAMES; f++) {
+            System.out.print(df.format((f*1.0/(FRAMES - 1))*100) + "%\r");
+            for (int n = 0; n < KEYS; n++) {
+                if (nc.getAt(n).getAt(f)) {
+                    if (!nc.getAt(n).getAt(f-1)) { //START NOTE IF ITS [f,t]
+                        mm = new ShortMessage();
+                        mm.setMessage(0x90,n+31,0x60);
+                        me = new MidiEvent(mm,(long)1+f*10);
+                        t.add(me);
+                    }
+                } else {
+                    if (nc.getAt(n).getAt(f-1)) { //END NOTE IF ITS [t,f]
+                        mm = new ShortMessage();
+                        mm.setMessage(0x80,n+31,0x40);
+                        me = new MidiEvent(mm,(long)1+f*10);
+                        t.add(me);
+                    }
+                }
+            }
+            Thread.sleep(40);
+        }
+        System.out.println("Writer finished, exporting...");
+        mt = new MetaMessage();
+        byte[] bet = {}; // empty array
+        mt.setMessage(0x2F,bet,0);
+        me = new MidiEvent(mt, (long)140);
+        t.add(me);
+        File fi = new File("midifile.mid");
+        MidiSystem.write(s,1,fi);
+        System.out.println("Midi created");
 
         System.out.println("Initializing Midi");
         Synthesizer midiSynth = MidiSystem.getSynthesizer();
